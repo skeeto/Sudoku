ede8327b019962f624df17f9a696f3e667e933db
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-19 19:45:21 -0400

    Added some things to TimeoutException.

--- src/com/nullprogram/sudoku/TimeoutException.java
+++ src/com/nullprogram/sudoku/TimeoutException.java
@@ -1,5 +1,9 @@
 package com.nullprogram.sudoku;
 
+/**
+ * Exception for when generating a new puzzle is taking too long.
+ */
 public class TimeoutException extends Exception {
 
+    private static final long serialVersionUID = 5617563511003836708L;
 }
